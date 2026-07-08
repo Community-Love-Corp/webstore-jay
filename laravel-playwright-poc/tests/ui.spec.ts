1764f11f248c2e91888a5e68ddd08449ae660207
Jyotirmay Sarna <jyotirmay@kali3.internal.systematicdefence.tech>
2026-07-09 11:32:09 +1200

    See Readme.MD Version 1.04

--- laravel-playwright-poc/tests/ui.spec.ts
+++ laravel-playwright-poc/tests/ui.spec.ts
@@ -4,3 +4,19 @@ test("homepage loads", async ({ page }) => {
 	await page.goto('http://127.0.0.1:8000');
 	await expect(page.locator("h1")).toContainText("Shop");
 });
+
+test("login form works", async ( {page} ) => {
+	await page.goto("http://127.0.0.1:8000");
+	//if email input element had id=email, then this would work:
+	//await page.fill("#email", "user@example.com");
+	await page.fill('input[name="email"]', 'user@example.com');
+	await page.fill('input[name="password"]', 'password');
+	const loginButton = page.getByRole("button", { name: "Login"} );
+	await loginButton.click();
+	
+//	await page.fill('input[name="password"]', 'password');
+
+//	await page.click('button[type="submit"]');
+
+	await expect(page).toHaveURL('http://127.0.0.1:8000/dashboard');
+});
